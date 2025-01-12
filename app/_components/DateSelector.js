a15b389e0f6c975a4df1f2baefb7589d9e01659d
Alireza Jalili <alirlikh@gmail.com>
2025-01-12 12:42:50 +0330

    fix: date picker

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -2,7 +2,7 @@
 
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
+import "react-day-picker/style.css";
 import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
