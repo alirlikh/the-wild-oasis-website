0932708386c6bed6295892d71f307823ec92df1a
Alireza Jalili <alirlikh@gmail.com>
2025-01-18 17:26:22 +0330

    feat: add delete reservation with indicator

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -3,10 +3,10 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import supabase from "./supabase";
+import { getBooking, getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
-
   if (!session) throw new Error("You must be logged in!");
 
   const nationalID = formData.get("nationalID");
@@ -25,6 +25,28 @@ export async function updateGuest(formData) {
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in!");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete!");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/acount/reservations");
 }
 
 export async function signInAction() {
