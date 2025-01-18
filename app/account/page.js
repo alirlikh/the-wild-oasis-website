4a0696c19d96bf3fd1df6af6b858451d37c9e24b
Alireza Jalili <alirlikh@gmail.com>
2025-01-18 11:08:45 +0330

    feat: show user name

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "account page",
 };
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">Wellcome</h2>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Wellcome {session?.user?.name}
+    </h2>
   );
 }
